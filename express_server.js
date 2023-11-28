3c0d8f8af047222ad9934da9ebd2ca9dba46ee71
YoasterToaster <joefoschia@gmail.com>
2023-11-27 19:04:38 -0500

    added hello_world.ejs, partials to the views and urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -23,6 +23,11 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
