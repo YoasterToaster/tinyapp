c18e1f7149a3e0e62fb0d7ec6541abd26d38dba2
YoasterToaster <joefoschia@gmail.com>
2023-11-29 19:26:50 -0500

    Added a registration page and will save a user as a cookie

--- express_server.js
+++ express_server.js
@@ -11,7 +11,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-let cookie = "";
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 // gets the current urls we have so we can display them all
 app.get("/urls", (req, res) => {
   const templateVars = { 
@@ -27,18 +39,36 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 // Sends "Hello!" to our root page
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 // Gets the 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
+app.get("/register", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+
+  // const obj = {
+  //   id: generateRandomString(),
+  //   email: users.email,
+  //   password: users.password
+  // }
+  res.render("urls_register", templateVars);
 });
+
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: "Hello World!" };
+//   res.render("hello_world", templateVars);
+// });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
@@ -49,9 +79,9 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -63,6 +93,11 @@ app.post("/urls", (req, res) => {
   res.redirect(req.body.longURL); // Respond with 'Ok' (we will replace this)
 });
 
+// app.get("/register", (req, res) => {
+//   // Add any necessary logic here
+//   res.render("urls_register");
+// });
+
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
@@ -73,6 +108,20 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[id] = req.body.longURL;
   res.redirect('/urls/');
 });
+
+app.post("/register", (req, res) => {
+  // Grab the email and password from the body
+  const { email, password } = req.body;
+  const id = generateRandomString();
+  const user = {id, email, password};
+    //Add to the users object
+  users[id] = user;
+  res.cookie("user_id", id);
+  
+  console.log(user);
+  res.redirect('/urls');
+});
+
 // Saves the login name and adds it as a cookie
 app.post("/login", (req, res) => {
   const username = req.body.username;
@@ -86,6 +135,7 @@ app.post("/logout", (req, res) => {
   res.clearCookie('username', req.body.username);
   res.redirect('/urls');
 });
+
 
 function generateRandomString() {
   const randomString = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
